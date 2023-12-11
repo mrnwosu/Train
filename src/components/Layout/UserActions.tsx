fc8422e62ad8d6f48a3c19e0b36a7ed71f27b478
Ike Nwosu <ike.nwosu.151@gmail.com>
2023-12-10 21:39:35 -0500

    Cleanup

--- src/components/Layout/UserActions.tsx
+++ src/components/Layout/UserActions.tsx
@@ -1,12 +1,11 @@
 import { signIn, signOut, useSession } from "next-auth/react";
 import Link from "next/link";
-import { useEffect, useRef, useState } from "react";
+import { useEffect, useState } from "react";
 
 export const UserActions = () => {
   const { data: sessionData } = useSession();
   const [shouldShowViewDashboardButton, setShouldShowViewDashboardButton] = useState(false);
   const pagesToHideViewDashboardButton = ["/dashboard"];
-  const counterRef = useRef(0);
 
   useEffect(() => {
     const pathName = window.location.pathname.toLowerCase();
@@ -39,95 +38,3 @@ export const UserActions = () => {
     </div>
   );
 };
-
-type ButtonProps = {
-  onClick?: () => void;
-  onHover?: () => void;
-  onIntesection?: () => void;
-  text: string;
-  className?: string | undefined;
-};
-
-function applyButtonProps(id: string, props: ButtonProps) {
-  const button = document.getElementById(id);
-  if (!button) return;
-
-  if (props.onClick) {
-    button.addEventListener("click", props.onClick);
-  }
-
-  if (props.onHover) {
-    button.addEventListener("mouseenter", props.onHover);
-  }
-
-  if (props.onIntesection) {
-    const observer = new IntersectionObserver((entries) => {
-      entries.forEach((entry) => {
-        if (entry.isIntersecting) {
-          if (!props.onIntesection) return;
-          props.onIntesection();
-        }
-      });
-    });
-
-    observer.observe(button);
-  }
-}
-
-
-
-function RoundedButton(props: ButtonProps) {
-
-    const guid = Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15);
-    useEffect(() => {
-      applyButtonProps(guid, props);
-    }, []);
-
-    return (
-      <button
-        className="rounded-full bg-yellow-700/50 px-6 py-2 font-semibold text-white no-underline transition hover:bg-yellow-500/50 active:bg-yellow-600/30 "
-      >
-        {props.text}
-      </button>
-    );
-}
-
-function GhostRoundedButton(props: ButtonProps) {
-
-  
-
-  return (
-    <button
-      id={guid}
-      className="rounded-full border-2 border-zinc-600 px-6 py-2 font-semibold text-white no-underline transition hover:bg-white/20 active:bg-white/30 "
-    >
-      {props.text}
-    </button>
-  );
-}
-
-
-function RoundSecondaryButton(props: ButtonProps) {
-  const guid = Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15);
-
-  useEffect(() => {
-    const button = document.getElementById(guid);
-    if (button) {
-      if (props.onClick) {
-        button.addEventListener("click", props.onClick);
-      }
-      if (props.onHover) {
-        button.addEventListener("mouseenter", props.onHover);
-      }
-    }
-  }, []);
-
-  return (
-    <button
-      id={guid}
-      className="rounded-full bg-white/10 px-6 py-2 font-semibold text-white no-underline transition hover:bg-white/20 active:bg-white/30 "
-    >
-      {props.text}
-    </button>
-  );
-}
